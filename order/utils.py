f13d65eeac7fb4c33b73295af9c335a626a35603
aleX-Xandr <72668137+aleX-Xandr@users.noreply.github.com>
2024-12-12 14:49:51 +0100

    Test button

--- order/utils.py
+++ order/utils.py
@@ -1,7 +1,10 @@
+from django.utils.html import format_html
 from paypal.standard.forms import PayPalPaymentsForm
-
 
 class CustomPayPalPaymentsForm(PayPalPaymentsForm):
 
     def get_html_submit_element(self):
-        return """<button type="submit">Procedi all&rsquo;acquisto</button>"""
+        return format_html(
+            """<input type="image" src="{0}" name="submit" alt="Buy it Now" />""",
+            self.get_image(),
+        )
