59214d497ca9750b8c2157a68cda9441829d4206
aleX-Xandr <72668137+aleX-Xandr@users.noreply.github.com>
2024-12-12 15:02:33 +0100

    Fix paypal styles

--- order/utils.py
+++ order/utils.py
@@ -2,6 +2,5 @@ from django.utils.safestring import mark_safe
 from paypal.standard.forms import PayPalPaymentsForm
 
 class CustomPayPalPaymentsForm(PayPalPaymentsForm):
-
     def get_html_submit_element(self):
-        return mark_safe("""<button type="submit">Procedi all&rsquo;acquisto</button>""")
+        return mark_safe("""<button type="submit" id="submit_paypal" style="background: transparent; border: unset; color: #fff; font-size: 17px; font-weight: 700; line-height: 18.46px;">Procedi all’acquisto</button>""")
