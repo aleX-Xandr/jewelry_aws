7801f0861f6817b51231ddeb8e49b10a96650834
aleX-Xandr <72668137+aleX-Xandr@users.noreply.github.com>
2024-12-12 14:46:07 +0100

    Fix paypal button

--- order/utils.py
+++ order/utils.py
@@ -4,4 +4,4 @@ from paypal.standard.forms import PayPalPaymentsForm
 class CustomPayPalPaymentsForm(PayPalPaymentsForm):
 
     def get_html_submit_element(self):
-        return """<button type="submit">Procedi all’acquisto</button>"""
+        return """<button type="submit">Procedi all&rsquo;acquisto</button>"""
