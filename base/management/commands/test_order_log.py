c064f807dde6a4b8871b6afcbddf44ad9fe8d09d
Alexandr Glasov <alexandr.glasov@data-ox.com>
2025-05-19 19:15:40 +0200

    Fix bug

--- base/management/commands/test_order_log.py
+++ base/management/commands/test_order_log.py
@@ -3,29 +3,12 @@ from django.core.management import BaseCommand
 
 from jewelry import settings
 from order.models import Order
+from order.hooks import order_log
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        order = Order.objects.filter().first()
+        order = Order.objects.filter().all()[-1]
         print(order)
-
-        positions_text = ""
-        for product in order.products.all():
-            text = f"Название: {product.product.name}\n" \
-                   f"Стоимость: {product.get_price()} ({product.quantity} шт.)\n\n"
-            positions_text += text
-
-        text = f"Новый заказ #{str(order.id).split('-')[0]}\n\n" \
-               f"Стоимость: {order.get_price()}\n\n" \
-               f"Товары:\n\n{positions_text}" \
-
-        print(text)
-
-        url = settings.TELEGRAM_LOG_URL
-        data = {
-            "chat_id": settings.TELEGRAM_CHAT_ID,
-            "text": text,
-        }
-        response = requests.post(url, json=data).text
-        print(response)
+        order_log(order)
+        
